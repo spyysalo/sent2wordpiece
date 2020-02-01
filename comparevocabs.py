47ba44e4bb4faa50bc617a7da93987f94a934d3f
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2020-02-01 16:27:20 +0200

    Add --reference-text argument

--- comparevocabs.py
+++ comparevocabs.py
@@ -4,7 +4,7 @@ import sys
 import os
 import re
 
-from collections import OrderedDict
+from collections import Counter, OrderedDict
 from itertools import combinations
 from random import sample
 from logging import warning
@@ -22,6 +22,8 @@ UNUSED_RE = re.compile(r'^\[unused[0-9]+\]$')
 def argparser():
     from argparse import ArgumentParser
     ap = ArgumentParser()
+    ap.add_argument('-t', '--reference-text', default=None,
+                    help='Reference text to compare against')
     ap.add_argument('vocab', nargs='+', help='BERT vocabulary')
     return ap
 
@@ -70,14 +72,44 @@ def max_sample(population, k):
     return sample(population, min(len(population), k))
 
 
-def compare(vocab1, vocab2, name1, name2):
+def compare(vocab1, vocab2, name1, name2, token_counts):
     v1 = set(vocab1)
     v2 = set(vocab2)
     print('{} ({}) / {} ({}): overlap {} ({:.1%}/{:.1%})'.format(
         name1, len(v1), name2, len(v2), len(v1&v2), len(v1&v2)/len(v1),
         len(v1&v2)/len(v2)))
-    print('{} \ {}: {}'.format(name1, name2, ' '.join(max_sample(v1-v2, 10))))
-    print('{} \ {}: {}'.format(name2, name1, ' '.join(max_sample(v2-v1, 10))))
+    max_examples = 10
+    if token_counts is None:
+        # No reference text, output a sample of differences
+        print('{} \ {}: {}'.format(name1, name2, ' '.join(
+            max_sample(v1-v2, max_examples))))
+        print('{} \ {}: {}'.format(name2, name1, ' '.join(
+            max_sample(v2-v1, max_examples))))
+    else:
+        # Print differences ordered by token frequency
+        v1count, v2count = 0, 0
+        for i, (t, _) in enumerate(sorted(token_counts.items(),
+                                          key=lambda i:-i[1]),
+                                   start=1):
+            if t in v1 and t not in v2 and v1count < max_examples:
+                print('{} \ {}: {} (rank {})'.format(name1, name2, t, i))
+                v1count += 1
+            if t in v2 and t not in v1 and v2count < max_examples:
+                print('{} \ {}: {} (rank {})'.format(name2, name1, t, i))
+                v2count += 1
+            if v1count >= max_examples and v2count >=max_examples:
+                break
+
+
+def basictoken_counts(fn):
+    counts = Counter()
+    with open(fn) as f:
+        for ln, l in enumerate(f, start=1):
+            l = l.rstrip()
+            for t in basic_tokenize(l):
+                if t and not t.isspace():
+                    counts[t] += 1
+    return counts
 
 
 def main(argv):
@@ -86,12 +118,16 @@ def main(argv):
         print('require at least 2 vocabs to compare', file=sys.stderr)
         return 1
     vocabs = OrderedDict([(p, load_vocab(p)) for p in args.vocab])
+    if args.reference_text is None:
+        reference_counts = None
+    else:
+        reference_counts = basictoken_counts(args.reference_text)
     for k in vocabs:
         vocabs[k] = filter_special(vocabs[k], k)
     for k in vocabs:
         check_vocab(vocabs[k], k)
     for k1, k2 in combinations(vocabs.keys(), 2):
-        compare(vocabs[k1], vocabs[k2], k1, k2)
+        compare(vocabs[k1], vocabs[k2], k1, k2, reference_counts)
     return 0
 
 
